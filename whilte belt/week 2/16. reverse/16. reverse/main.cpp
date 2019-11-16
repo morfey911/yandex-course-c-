227b34b0c0f61a5888795cdaf8482c36f0f6d16f
Yurii Mamurko <yurii.mamurko@skylum.com>
2019-11-16 21:54:20 +0200

    add 16. reverse

--- whilte belt/week 2/16. reverse/16. reverse/main.cpp
+++ whilte belt/week 2/16. reverse/16. reverse/main.cpp
@@ -7,8 +7,23 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+void Reverse(vector<int>& v) {
+    const auto copy = v;
+    const auto size = copy.size();
+
+    for (size_t i = 0; i < size; ++i) {
+        v[i] = copy[size - i - 1];
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    
+    vector<int> numbers = {1, 5, 3, 4, 2};
+    Reverse(numbers);
+    // numbers должен оказаться равен {2, 4, 3, 5, 1}
+
     return 0;
 }
