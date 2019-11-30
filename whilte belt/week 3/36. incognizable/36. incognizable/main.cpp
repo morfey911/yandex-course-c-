f7fba320f06ad97267b98b6bd1a5a2522ebf547b
Yurii Mamurko <yurii.mamurko@skylum.com>
2019-11-30 21:34:33 +0200

    change 36. incognizable

--- whilte belt/week 3/36. incognizable/36. incognizable/main.cpp
+++ whilte belt/week 3/36. incognizable/36. incognizable/main.cpp
@@ -8,8 +8,26 @@
 
 #include <iostream>
 
+class Incognizable {
+public:
+    Incognizable() {};
+    Incognizable(int a) {
+        this->a = a;
+    }
+    Incognizable(int a, int b) {
+        this->a = a;
+        this->b = b;
+    }
+
+private:
+    int a, b;
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = {0};
+    Incognizable d = {0, 1};
+
     return 0;
 }
